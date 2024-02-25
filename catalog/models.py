2c91e94940bb6b909e2c1dde14e92b12af1bb731
Antoha87 <mak.anton87@gmail.com>
2024-02-25 16:25:07 +0200

    add catalog api

--- catalog/models.py
+++ catalog/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class Catalog(models.Model):
     name = models.CharField('Catalog', max_length=100)
+    active = models.BooleanField(default=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Catalog
+from .serializers import CatalogSerializer
 
-# Create your views here.
+
+class CatalogViewSet(viewsets.ModelViewSet):
+    queryset = Catalog.objects.all()
+    serializer_class = CatalogSerializer
