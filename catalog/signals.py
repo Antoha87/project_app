d15f6c25525b774ae2862dc00bbd30a9f7fea2e7
Antoha87 <mak.anton87@gmail.com>
2024-03-03 15:46:38 +0200

    fix

--- catalog/signals.py
+++ catalog/signals.py
@@ -1,11 +1,11 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .tasks import get_current_price
 from .models import Coin
 
 
-@receiver(signal=pre_save, sender=Coin)
-def pre_save_coin(sender, instance, created, **kwargs):
+@receiver(signal=post_save, sender=Coin)
+def post_save_coin(sender, instance, created, **kwargs):
     if created:
         name = instance.name
         price = get_current_price(name).delay()
