6cbdfa0cefba17ab97644917bd8ce6fab401e5d8
Antoha87 <mak.anton87@gmail.com>
2024-03-03 15:58:34 +0200

    dfdsdf

--- catalog/signals.py
+++ catalog/signals.py
@@ -8,6 +8,6 @@ from .models import Coin
 def post_save_coin(sender, instance, created, **kwargs):
     if created:
         name = instance.name
-        price = get_current_price(args=[name]).delay()
+        price = get_current_price.delay(name)
         instance.price = price
         instance.save()
